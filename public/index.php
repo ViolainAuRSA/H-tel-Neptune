15a0126a881cf538ef63b4d0bd20d3450380a3c2
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-11-26 16:22:37 +0100

    complet

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../Database.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Neptune</title>
+</head>
+<body>
+    <h1>Hello</h>
+</body>
+</html>
