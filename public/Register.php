e563ea3d49cce2181d50a2b7d229ac3d638f95e4
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-12-04 15:24:53 +0100

    v1

---
+++ public/Register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'enrigistrement</title>
+</head>
+<body>
+    <form action="auth.php" method="post">
+        Votre nom : <input type="text" name="nom">
+        <br>
+        Votre prénom : <input type="text" name="prenom">
+        <br>
+        Votre Mail : <input type="mail" name="mail">
+        <br>
+        Mot de passe : <input type="password" name="password">
+        <br>
+    </form>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -4,12 +4,17 @@ require_once __DIR__ . '/../Database.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Neptune</title>
 </head>
+
 <body>
     <h1>Hello</h>
+        <form action="#" method="post"></form>
+        <a href="Register.php">Mon lien</a>
 </body>
+
 </html>
