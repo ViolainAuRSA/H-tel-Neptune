cd85b3ce48eeb7a3a60eaa6e03f029ef9a44ad99
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-12-17 10:06:08 +0100

    ajout de la page de profil

---
+++ public/profile.php
@@ -0,0 +1,20 @@
+<?php
+    include_once 'include.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hôtel Neptune - Votre séjour de rêve</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+    <?php include 'header/header.php'; ?>
+    <main>
+        <h1>Profile</h1>
+    </main>
+    <?php include 'footer/footer.php'; ?>
+</body>
+</html>
